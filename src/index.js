f04b911ec5e3b9bffd81fc7c60641ee1622d7d00
Navalkar-Nikhil <120443609+Navalkar-Nikhil@users.noreply.github.com>
2023-05-09 16:29:17 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 //Write your code here
-
-
-
-
+ES6 Class                                                                                                                                                                                                       class API{
+    #secure;
+    url;
+    method;
+    constructor(url){
+        this.url=url;
+        if(url.includes("https")) this.#secure=true;
+        else this.#secure=false;
+        this.method="GET"
+    }
+    isSecure(){
+        return this.#secure;
+    }
+    updateUrl(url){
+        this.url=url
+        if(url.includes("https"))
+        this.#secure=true;
+        else
+        this.#secure=false;
+    }
+}
 
 //Do not remove this code
 module.exports = { API }
